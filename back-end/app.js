392b8d467481d5ac62aec01392ddb9dfda6b0cef
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-09-13 21:46:43 -0400

    index route for users created, controller imported into app.js to use users controllers middleware

--- back-end/app.js
+++ back-end/app.js
@@ -9,12 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ALL CONTROLLERS
+const usersController = require('./controllers/usersController');
+app.use('/users', usersController);
+
 // ROUTES 
 app.get("/", (req, res) => {
     res.send("Welcome to Adnan's GoldenSolutions Database!")
 });
 
-// CATCH ALL ROUTE
+// CATCH ALL ROUTE // THE 404 PAGE
 app.get("*", (req, res) => {
     res.status(404).send('Error: this page cannot be Found!')
 });
---
+++ back-end/controllers/usersController.js
@@ -0,0 +1,22 @@
+// CONFINGURATIONS
+const express = require('express');
+const users = express.Router();
+
+// IMPORTS OF ALL USER QUERIES
+
+
+
+                 //***ROUTES***// 
+
+// ALL USERS // THE INDEX ROUTE // 
+users.get('/', async (req, res) => {
+    const allUsers = await getAllUsers();
+    if (allUsers[0]) {
+        res.status(200).json(allUsers);
+    } else {
+        res.status(500).json( {error: 'server error'});
+    }
+});
+
+
+
