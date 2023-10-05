b92304d6db03d3677cce486d842a2974f8e89029
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-04 23:40:34 -0400

    created a card to be used all across the project next is styling the card to display it

---
+++ front-end/src/components/cards/Cards.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cards = (props) => {
+  return (
+    <>
+      <div className="cards">
+        <Link className="cards__link" to={props.path}>
+          <figure className="cards__pic" data-category={props.label}>
+            <img className="cards__image" alt="photo" src={props.src} />
+          </figure>
+          <div className="card__info">
+            <h5 className="cards__text">{props.text}</h5>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
