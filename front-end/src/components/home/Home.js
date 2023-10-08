c3593ec23a2c529ddcb6afb957f27851abe507ac
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-07 21:26:32 -0400

    cards being displayed on the homepage

--- front-end/src/components/home/Home.js
+++ front-end/src/components/home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import HomePage from './HomePage';
 
 const Home = () => {
   return (
     <>
     <div className='home'>
-        <h1>WELCOME TO GOLDEN SOLUTIONS!</h1>
+        {/* <h1>WELCOME TO GOLDEN SOLUTIONS!</h1> */}
         <video src='/video/volunteer.mov' autoPlay loop muted />
     </div>
+    <HomePage />
     </>
   )
 }
